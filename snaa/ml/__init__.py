ff3a99856d3e2eee6167287d3381a765cc4080b6
Thomas Pircher <pircher.th@googlemail.com>
2021-04-02 13:36:03 +0200

    start clean up for github

---
+++ snaa/ml/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+project:    snaa
+filename:	__init__.py
+author:		pi
+date:		2020-11-10
+version:	
+
+description:
+
+CHANGE LOG:
+
+'''
--- utils.py
+++
@@ -1,1 +0,0 @@
-from eventsearch.utils import *
